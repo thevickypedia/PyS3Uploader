a3fc1234b69b545be1ac825cda836232ca068146
Vignesh Rao <svignesh1793@gmail.com>
2025-11-06 20:43:28 -0600

    perf: Replace ``RepeatedTimer`` class with a non-blocking, daemon-thread version

--- pys3uploader/timer.py
+++ pys3uploader/timer.py
@@ -1,4 +1,5 @@
-from threading import Timer
+import logging
+import threading
 from typing import Any, Callable, Dict, Tuple
 
 
@@ -15,6 +16,7 @@ class RepeatedTimer:
         function: Callable,
         args: Tuple = None,
         kwargs: Dict[str, Any] = None,
+        logger: logging.Logger = None,
     ):
         """Repeats the ``Timer`` object from threading.
 
@@ -23,32 +25,33 @@ class RepeatedTimer:
             function: Function to trigger with intervals.
             args: Arguments for the function.
             kwargs: Keyword arguments for the function.
+            logger: Logger instance.
         """
-        self._timer = None
         self.interval = interval
         self.function = function
         self.args = args or ()
         self.kwargs = kwargs or {}
-        self.is_running = False
+        self.logger = logger or logging.getLogger(__name__)
+        self.thread = None
+        self._stop_event = threading.Event()
 
     def _run(self):
         """Triggers the target function."""
-        self.is_running = False
-        self.start()
-        self.function(*self.args, **self.kwargs)
+        while not self._stop_event.wait(self.interval):
+            try:
+                self.function(*self.args, **self.kwargs)
+            except Exception as error:
+                self.logger.error("Error in RepeatedTimer function [%s]: %s", self.function.__name__, error)
 
     def start(self):
         """Trigger target function if timer isn't running already."""
-        if not self.is_running:
-            self._timer = Timer(self.interval, self._run)
-            self._timer.start()
-            self.is_running = True
+        if self.thread and self.thread.is_alive():
+            return
+        self.thread = threading.Thread(target=self._run, daemon=True)
+        self.thread.start()
 
-    def stop(self):
+    def stop(self, timeout: int = 3):
         """Stop the timer and cancel all futures."""
-        self._timer.cancel()
-        self.is_running = False
-
-    def cancel(self):
-        """Initiate cancellation."""
-        self.stop()
+        self._stop_event.set()
+        if self.thread:
+            self.thread.join(timeout=timeout)
